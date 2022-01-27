267d0fef1593e0d1dce38e9f94e06ee6ef412f06
LryIII <2123358697@qq.com>
2022-01-27 14:49:09 +0800

    add: 大致实现了一个简单的排行榜界面

--- lib/pages/rank/rank_network.dart
+++ lib/pages/rank/rank_network.dart
@@ -2,12 +2,12 @@ import 'package:dio/dio.dart';
 
 class RankMyItem {
   Dio dio=Dio();
-  void addItem(String username,double time) async{
+  Future<void> addItem(String name,double time) async{
     try{
       var response=await dio.post(
         'http://101.43.148.116:9002/v1/admin/twt/sort',
         queryParameters: {
-          'name':username,
+          'name':name,
           'time':'$time',
         },
       );
@@ -20,8 +20,7 @@ class RankMyItem {
 
     }
   }
-  Future<Map> getRank(int head,int tail) async{
-
+  Future<Map> getAllRank(int head,int tail) async{
     try{
       var response=await dio.get(
         'http://101.43.148.116:9002/v1/admin/twt/list',
@@ -30,11 +29,75 @@ class RankMyItem {
           'tail': '$tail',
         }
       );
-      print(response.data);
+      //print(response.data);
       return response.data;
     }catch(e){
       print(e);
       return {};
+    }
+  }
+  Future<Map> getOneTime(String name) async{
+    try{
+      var response=await dio.get(
+        'http://101.43.148.116:9002/v1/admin/twt/sort',
+        queryParameters: {
+          'name':name,
+        }
+      );
+      return response.data;
+    }
+    catch(e){
+      return {};
+    }
+  }
+  Future<double> getOneOnlyTime(String name) async{
+    try{
+      var response=await dio.get(
+        'http://101.43.148.116:9002/v1/admin/twt/sort/time',
+        queryParameters: {
+          'name':name,
+        }
+      );
+      return response.data['result'].toDouble();
+    }catch(e){
+      return -0.01;
+    }
+  }
+  Future<bool> isInRank(String name) async{
+    try{
+      var response=await dio.get(
+        'http://101.43.148.116:9002/v1/admin/twt/sort/exist',
+        queryParameters: {
+          'name':name,
+        }
+      );
+      return response.data['result']=='1';
+    }catch(e){
+
+      return false;
+    }
+  }
+  Future<int> getAllNum()async{
+    try{
+      var response=await dio.get(
+        'http://101.43.148.116:9002/v1/admin/twt/sorts/num',
+      );
+      return response.data['result'].toInt();
+    }catch(e){
+      return -1;
+    }
+  }
+  Future<int> getOneRank(String name)async{
+    try{
+      var response=await dio.get(
+        'http://localhost:9002/v1/admin/twt/ranking',
+        queryParameters:{
+          'name':name,
+        }
+      );
+      return response.data['result'].toInt();
+    }catch(e){
+      return -1;
     }
   }
 }
